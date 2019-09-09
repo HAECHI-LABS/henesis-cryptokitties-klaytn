694369dbcd1c9c6ae7d1ad5a6b0a0901e8a6ee86
JhChoy <jchoyeclipse@gmail.com>
2019-09-09 19:15:30 +0900

    feat: add answer

--- backend/src/event.js
+++ backend/src/event.js
@@ -1,7 +1,41 @@
 import Henesis from '@haechi-labs/henesis-sdk-js'
+import { eventNames } from 'cluster';
 
 export default async function ({config, model}) {
-    /*
-        Your code starts here!
-    */
+  const henesis = new Henesis();
+  const subscription = await henesis.subscribe(
+    config.integrationId,
+    "streamedBlock"
+  )
+  subscription.on('message', async (message) => {
+    const events = messageToEvents(message)
+    events.forEach(event => model.save(event))
+    console.log(`data received, event:${events}`)
+    message.ack();
+  });
+
+  subscription.on('error', err => {
+    console.error(err);
+  });
+
+  //parsing logic
+  function messageToEvents(message) {
+    const events = message.data[0].events;
+    const blockMeta = message.data[0].blockMeta
+    return events.map( event =>{
+      return {
+        event: event.eventName.split('(')[0],
+        contract: event.contractName,
+        transactionHash: event.transaction.hash,
+        args: dataToArgs(event.data),
+        blockMeta
+      } })
+    function dataToArgs(data) {
+      const res = {}
+      for (let item of data) {
+        res[item.name] = item.value
+      }
+      return res
+    }
+  }
 }
